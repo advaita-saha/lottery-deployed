15a9b655018ddca86d58805245b085ebd2221dff
Advaita Saha <adv11sphs@gmail.com>
2023-01-28 00:48:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React,{Component} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+  
+  constructor (props){
+    super(props);
+    this.state = {
+      "manager" : '', 
+      players   : [], 
+      balance   : '',
+      value     : '',
+      message   : '',
+    };
+  }
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();//never set  manager var
+    const player  = await lottery.methods.playersData().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({
+      manager : manager,
+      players : player,
+      balance : balance,
+    });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message : 'Waiting on transaction success .....' });
+
+    await lottery.methods.enter().send({
+      from : accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether'),
+    });
+
+    this.setState( {message: 'Congratulations!!! You have successfully entered the lottery!' })
+
+  }
+
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({message : 'Picking a RANDOM winner........just hold on few secs.....'})
+    await lottery.methods.pickWinner().send({
+      from : accounts[0],
+    });
+    this.setState({message : 'A winner has been picked! And he has recieved the money'})
+  }
+
+  render() {
+    return (
+      <div>
+      <h2>Lottery contract - Shardeum</h2>
+      <p>This contract is managed by {this.state.manager}</p>
+      <p>There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} SHM!!!</p>
+      <hr/>
+      <form onSubmit={this.onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of SHM to enter : </label>
+          <input
+            value={this.state.value}
+            onChange={event => this.setState({ value : event.target.value })}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+      <hr/>
+      <h4>Ready to Pick a Winner? </h4>
+      <button onClick={this.onClick} >Pick a Winner!</button>
+      <hr/>
+      <h3>{this.state.message}</h3>
+    </div>
+    );
+  }
+}
+
+
+
+export default App;
---
+++ src/lottery.js
@@ -0,0 +1,71 @@
+import web3 from "./web3";
+
+const address = "0xC00568E833bfdb24B1532c575A926207bB54B4b8";
+const abi = [
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "playersData",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+export default new web3.eth.Contract(abi, address);
